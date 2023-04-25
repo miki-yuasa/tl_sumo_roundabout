847d13a7a1cf89dbc31e5ff6497fff45230e216d
Mikihisa Yuasa <myuasa2@illinois.edu>
2023-04-25 06:22:37 -0500

    fix: correct numpy array conversion

--- tl_sumo_roundabout/evaluation.py
+++ tl_sumo_roundabout/evaluation.py
@@ -10,6 +10,15 @@ def simulate_mode(
     obs, info = env.reset()
     obs_list: list[ObsDict] = [obs]
     for i in range(1000):
+        obs["ego_speed"] = obs["ego_speed"].reshape(
+            1,
+        )
+        obs["t_0_speed"] = obs["t_0_speed"].reshape(
+            1,
+        )
+        obs["t_1_speed"] = obs["t_1_speed"].reshape(
+            1,
+        )
         action, _states = model.predict(obs, deterministic=True)
         obs, rewards, terminated, truncated, info = env.step(action)
         if env._is_gui_rendered and render_path is not None:
