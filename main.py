383d26b485397d426e466d4dfe91ec5bb8db93a7
Mikihisa Yuasa <mik0915yua@outlook.com>
2023-04-25 01:48:43 -0500

    feat: edit main fn

--- main.py
+++ main.py
@@ -3,39 +3,43 @@ from numpy import ndarray
 
 from matplotlib import pyplot as plt
 import seaborn as sns
+import torch
 
 from tl_sumo_roundabout.train import train_rl_agent
-from tl_sumo_roundabout.utils import create_env, spec2title
+from tl_sumo_roundabout.utils import spec2title
+from tl_sumo_roundabout.env import create_env
 
 
 experiment: int = 0
 spec: str = "F(psi_a) & G(psi_b | (psi_c & psi_d))"
 env_name: str = "2_veh_all_off"
-num_actions: int = 20
-max_steps: int = 300
-config_path: str = "sumo/configs/2_veh/roundabout.sumocfg"
+num_actions: int = 10
+max_steps: int = 200
+config_path: str = "configs/sumo/2_veh/roundabout.sumocfg"
+step_length: float = 0.5
 ego_aware_dist: float = 200.0
 others_speed_mode: int = 32
 
 num_replicates: int = 3
-total_timesteps: int = 100
-num_envs: int = 10
+total_timesteps: int = 1
+num_envs: int = 1
+gpu: str = "cuda:0"
 
-window: int = round(total_timesteps / 100)
+window: int = 100  # round(total_timesteps / 100)
 lc_plot_max_x: int = 900
 
 model_path: str = f"out/model/model_{spec2title(spec)}_{experiment}"
 model_paths: list[str] = [f"{model_path}_{i}" for i in range(num_replicates)]
-learning_curve_path: str = (
-    f"out/learning_curve/learning_curve_{spec2title(spec)}_{experiment}.png"
-)
 learning_curve_paths: list[str] = [
-    f"out/learning_curve/learning_curve_{spec2title(spec)}_{experiment}_{i}.png"
+    f"out/plot/learning_curve/learning_curve_{spec2title(spec)}_{experiment}_{i}.png"
     for i in range(num_replicates)
 ]
 
 sns.set(style="whitegrid")
 plt.rcParams["font.family"] = "Times New Roman"
+
+device = torch.device(gpu if torch.cuda.is_available() else "cpu")
+torch.cuda.set_device(device)
 
 lcs: list[tuple[ndarray, ndarray]] = []
 
@@ -51,6 +55,7 @@ for i in range(num_replicates):
             num_actions,
             max_steps,
             config_path,
+            step_length=step_length,
             ego_aware_dist=ego_aware_dist,
             others_speed_mode=others_speed_mode,
         )
