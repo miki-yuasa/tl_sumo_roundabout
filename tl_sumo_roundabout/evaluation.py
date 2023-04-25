964af24b1b176cbaa8bd6d13af056a32a0d54ee7
Mikihisa Yuasa <myuasa2@illinois.edu>
2023-04-25 05:24:21 -0500

    feat: add model simulator

---
+++ tl_sumo_roundabout/evaluation.py
@@ -0,0 +1,31 @@
+from stable_baselines3 import PPO
+from sumo_gym.envs.types import InfoDict, ObsDict
+
+from tl_sumo_roundabout.env import TlRoundaboutEnv
+
+
+def simulate_mode(
+    model: PPO, env: TlRoundaboutEnv, render_path: str | None = None
+) -> tuple[ObsDict, bool]:
+    obs, info = env.reset()
+    obs_list: list[ObsDict] = [obs]
+    for i in range(1000):
+        action, _states = model.predict(obs, deterministic=True)
+        obs, rewards, terminated, truncated, info = env.step(action)
+        if env._is_gui_rendered and render_path is not None:
+            env.render(f"{render_path}_{i}.png")
+        else:
+            pass
+
+        obs_list.append(obs)
+
+        if terminated or truncated:
+            break
+        else:
+            pass
+
+    collided: bool = info["ego_collided"]
+
+    env.close()
+
+    return obs_list, collided
